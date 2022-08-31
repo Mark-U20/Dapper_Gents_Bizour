9e5c3724407c8e5ec1d232b1e06339da8f0bd7de
fixedOtter <fo1152rc@go.minnstate.edu>
2022-08-31 15:14:42 -0500

    added to typeDefs

--- server/schemas/typeDefs.js
+++ server/schemas/typeDefs.js
@@ -1,14 +1,48 @@
 const { gql } = require("apollo-server-express");
 
+/* FIXME:
+* do we need to add something to the user
+* for the cart items? do we need a model?
+* -fixedOtter
+*/
+
 const typeDefs = gql`
-   
+  type User {
+    _id: ID!
+    email: String!
+    listings: [Listing]
+    **
+  }
 
-    type Query{
-    }
+  type Listing {
+    _id: ID!
+    item_name: String!
+    description: String!
+    category: String!
+    quantity: Number!
+    price: Number!
+    listing_author: User
+    reviews: [Review]
+  }
 
-    type Mutation{
-   
-    }
-    `;
+  type Review {
+    _id: ID!
+    review_text: String!
+    review_author: User
+  }
+
+  type Query{
+    getUser: User
+    getUsers: [User]
+    getListing: Listing
+    getListings: [Listing]
+  }
+
+  type Mutation{
+    addUser(email: String!, password: String!): Auth
+    loginUser(email: String!, password: String!): Auth
+    addListing(item_name: String!, description: String!, category: String!, quantity: Number!, price: Number!): Listing
+  }
+`;
 
 module.exports = typeDefs;
