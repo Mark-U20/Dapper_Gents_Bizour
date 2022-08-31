00252d001a4c8c0f0f777688c8d0ba01c2ab2b37
joostyn <justyn.helgeson@gmail.com>
2022-08-31 16:01:59 -0500

    added some resolvers

--- server/schemas/resolvers.js
+++ server/schemas/resolvers.js
@@ -1,6 +1,43 @@
+const { Cart, Listing, Review, User } = require('../models');
+
 const resolvers = {
-  Query: {},
-  Mutation: {},
+  Query: {
+    async getUser(_, { userId }) {
+      return await User.findOne({ _id: userId });
+    },
+    async getUsers() {
+      return await User.find();
+    },
+    async getListing(_, { listingId }) {
+      return await Listing.findOne({ _id: listingId });
+    },
+    async getListings() {
+      return await Listing.find();
+    },
+  },
+  Mutation: {
+    // async addUser(_, { email, password }) {
+    //   return User.create({ email, password });
+    // },
+    // async loginUser(_, { email, password }){
+
+    // },
+    async addListing(_, {
+      item_name,
+      description,
+      category,
+      quantity,
+      price,
+    }) {
+      return Listing.create({
+        item_name,
+        description,
+        category,
+        quantity,
+        price,
+      })
+    }
+  },
 };
 
 module.exports = resolvers;
