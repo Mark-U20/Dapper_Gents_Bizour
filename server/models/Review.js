72c82fa77ef17ee65b30449e4c78994f88a8082b
joostyn <justyn.helgeson@gmail.com>
2022-08-31 19:15:53 -0500

    Make changes to Review and Listing models

--- server/models/Review.js
+++ server/models/Review.js
@@ -2,6 +2,17 @@ const { Schema, model, SchemaTypes } = require('mongoose');
 
 const reviewSchema = new Schema(
     {
+        review_rating: {
+            type: Number,
+            required: [true, 'You must give a score between 0 and 10'],
+            min: 0,
+            max: 10,
+            
+        },
+        review_title: {
+            type: String,
+            required: true,
+        },
         review_text: {
             type: String,
             required: true,
