b7a9e3c3fa5f84463396c61429ce9b81bda23010
joostyn <justyn.helgeson@gmail.com>
2022-09-01 14:22:45 -0500

    working on Pokemon page

--- client/src/pages/Pokemon.js
+++ client/src/pages/Pokemon.js
@@ -1,11 +1,13 @@
 import ShowListings from '../components/ShowListings'
+import pageLogo from '../images/justynsPokemonCards.png'
 function Pokemon() {
 
 
     return (
-        <>
+        <div>
+            <img src={pageLogo} className="page-logo"/>
             <ShowListings />
-        </>
+        </div>
     )
 }
 
