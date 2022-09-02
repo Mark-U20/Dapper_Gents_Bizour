93a542c70902cdd3e5955b8a9754df57a1183807
fixedOtter <fo1152rc@go.minnstate.edu>
2022-09-01 21:02:37 -0500

    did some with graphql queries; moved computerParts to computerFarts; added main div class for styling

--- client/src/pages/index.js
+++ client/src/pages/index.js
@@ -1,7 +1,7 @@
-import Landing from './Landing'
-import Pokemon from './Pokemon'
-import Random from './Random'
-import ComputerParts from './ComputerParts'
+import Landing from './Landing';
+import Pokemon from './Pokemon';
+import Random from './Random';
+import ComputerParts from './computerParts';
 import UserForm from './UserForm';
 
 export {
