e67daf35eaf3bd353fb185077abdc408da23df35
fixedOtter <fo1152rc@go.minnstate.edu>
2022-09-01 21:18:16 -0500

    setup the initial gungun computerFarts

--- client/src/pages/computerFarts/index.js
+++ client/src/pages/computerFarts/index.js
@@ -1,16 +1,14 @@
-import { useQuery } from '@apollo/client';
-import { useState } from 'react';
-import { GET_LISTINGS } from '../../utils/queries';
+import { ShowListings } from '../../components';
 import './style.css';
 
 export default function ComputerFarts() {
-  const [queryData, setQueryData] = useState(useQuery(GET_LISTINGS));
 
 
 
   return (
     <>
       <h1>Yes this is text</h1>
+      <ShowListings />
     </>
-  )
+  );
 }
