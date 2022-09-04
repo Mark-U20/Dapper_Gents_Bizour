8e3eaa6190a17403db11bd284d431e5c844334d1
LSton40 <logan.sutton@gmail.com>
2022-09-04 12:38:27 -0500

    Pixiplay

--- client/src/App.js
+++ client/src/App.js
@@ -56,7 +56,7 @@ function App() {
 
           {/* <Route render={() => <h1 className='display-2'>Wrong page!</h1>} /> */}
         </Routes>
-        <canvas id="container"></canvas>{' '}
+        {/* <canvas id="container"></canvas>{' '} */}
       </main>
       <Footer />
     </>
