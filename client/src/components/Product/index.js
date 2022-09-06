b6ba9f525fa4b68b02201fc8056b72d1d69d77c3
joostyn <justyn.helgeson@gmail.com>
2022-09-06 15:00:42 -0500

    Did some product page fixes

--- client/src/components/Product/index.js
+++ client/src/components/Product/index.js
@@ -1,10 +1,10 @@
-import {useState, useEffect} from 'react'
+import { useState, useEffect } from 'react'
 import { useQuery, useMutation } from '@apollo/client';
 import { useParams } from 'react-router-dom';
 import { GET_LISTING } from "../../utils/queries";
 import { ADD_TO_CART } from "../../utils/mutations";
 import AuthService from '../../utils/auth';
-import {saveItemIds, getCartItemIds} from '../../utils/localStorage';
+import { saveItemIds, getCartItemIds } from '../../utils/localStorage';
 import './style.css'
 
 const Product = () => {
@@ -22,7 +22,7 @@ const Product = () => {
     });
 
     const handleCartSave = async (prodID) => {
-        
+
         const token = AuthService.loggedIn() ? AuthService.getToken() : null;
 
         if (!token) {
@@ -32,7 +32,7 @@ const Product = () => {
         console.log(data.getListing.title)
         console.log(prodID)
 
-        const shoppingCart = await saveToCart({variables: {listingId: data.getListing._id}});
+        const shoppingCart = await saveToCart({ variables: { listingId: data.getListing._id } });
 
         setSavedItemIds([...savedItemIds, prodID])
     }
@@ -45,25 +45,25 @@ const Product = () => {
 
             {data && (
 
-                    <div className='ui card'>
-                        <div className='image'>
-                            <img src={data.getListing.image} alt={data.getListing.title} />
-                        </div>
-                        <div className='content'>
-                            <h1 className='header'>{data.getListing.title}</h1>
-                        </div>
-                        <div className="meta">
-                            <span className="date">{data.getListing.category}</span>
-                        </div>
-                        <div className='description'>
-                            <p>{data.getListing.description}</p>
-                            <p>Quantity: {data.getListing.quantity}</p>
-                            <p>Price: ${data.getListing.price}</p>
-                            <p>Seller: {data.getListing.listing_author.email}</p>
-                        </div>
-                        {/* <ProductReviews /> */}
-                        <button className="ui primary button" onClick={() => handleCartSave(data.getListing._id)}>Add to Cart</button>
+                <div className='ui card'>
+                    <div className='image'>
+                        <img src={data.getListing.image} alt={data.getListing.title} />
                     </div>
+                    <div className='content'>
+                        <h1 className='header'>{data.getListing.title}</h1>
+                    </div>
+                    <div className="meta">
+                        <span className="date">{data.getListing.category}</span>
+                    </div>
+                    <div className='description'>
+                        <p>{data.getListing.description}</p>
+                        <p>Quantity: {data.getListing.quantity}</p>
+                        <p>Price: ${data.getListing.price}</p>
+                        <p>Seller: {data.getListing.listing_author}</p>
+                    </div>
+                    {/* <ProductReviews /> */}
+                    <button className="ui primary button" onClick={() => handleCartSave(data.getListing._id)}>Add to Cart</button>
+                </div>
             )}
         </div>
     )
