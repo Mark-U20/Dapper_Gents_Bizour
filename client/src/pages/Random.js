0702d2ef5cdaa100f84b3e878ccdf015bebb68b5
LSton40 <logan.sutton@gmail.com>
2022-09-06 20:44:26 -0500

    Random seed and formatting

--- client/src/pages/Random.js
+++ client/src/pages/Random.js
@@ -1,24 +1,24 @@
 import { useState } from "react";
 import ProductCard from "../components/ProductCard";
 import { ShowRandomListings } from '../components';
-// import { random_inventory } from "../store_inventory/random_inventory";
 import {motion} from 'framer-motion';
 
 
 
 
 function Random() {
-    // const [inventory, setInventory] = useState(random_inventory);
-
 
     return (
         <motion.section
             initial={{opacity: 0, transition: {duration: 0.5}}}
             animate={{opacity: 1}}
             exit={{opacity: 0, transition: {duration: .5}}}
+            className="store-section"
         >
-            <h1>Random Stuff</h1>
-            <p>Your home for random crap</p>
+            <div className="heading">
+                <h1>Random Stuff</h1>
+                <p>Your home for random crap</p>
+            </div>
             <ShowRandomListings />
         </motion.section>
     )
